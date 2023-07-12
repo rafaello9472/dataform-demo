96265894a544bd5b9b0fe70ee2e25fa8fd33ebd7
Rafał Manda <rafalmanda@cloud4datascience.pl>
2023-07-12 17:45:47 +0000

    Add document_with_js branch

---
+++ includes/docs.js
@@ -0,0 +1,27 @@
+const user_id = `A unique identifier for a user`;
+const age= `The age of a user`;
+const creation_date= `The date this user signed up`;
+const user_tenure= `The number of years since the user's creation date`;
+const badge_count= `The all-time number of badges the user has received`;
+const questions_and_answer_count= `The all-time number of questions and answers the user has created`;
+const question_count= `The all-time number of questions the user has created`;
+const answer_count= `The all-time number of answers the user has created`;
+const last_badge_received_at= `The time the user received their most recent badge`;
+const last_posted_at= `The time the user last posted a question or answer`;
+const last_question_posted_at= `The time the user last posted an answer`;
+const last_answer_posted_at= `The time the user last posted a question`;
+
+module.exports = {
+   user_id,
+   age,
+   creation_date,
+   user_tenure,
+   badge_count,
+   questions_and_answer_count,
+   question_count,
+   answer_count,
+   last_badge_received_at,
+   last_posted_at,
+   last_question_posted_at,
+   last_answer_posted_at,
+};
